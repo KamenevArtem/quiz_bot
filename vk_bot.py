8bcff6d2befbc5e215290c241e5d916f35956f1a
KamenevArtem <kamenevart94@gmail.com>
2023-11-27 22:01:17 +0300

    add: keyboard to vk_bot

--- vk_bot.py
+++ vk_bot.py
@@ -4,14 +4,21 @@ import os
 from dotenv import load_dotenv
 
 import vk_api as vk
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 
 def echo(event, vk_api):
+    keyboard = VkKeyboard(one_time=True)
+    keyboard.add_button('Новый вопрос', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button('Сдаться', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_line()
+    keyboard.add_button('Посмотреть счёт', color=VkKeyboardColor.PRIMARY)
     vk_api.messages.send(
         user_id=event.user_id,
         message=event.text,
-        random_id=random.randint(1,1000)
+        random_id=random.randint(1,1000),
+        keyboard=keyboard.get_keyboard()
     )
 
 
