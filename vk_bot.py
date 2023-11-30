b35865329af8a67e8441cabeeb26286cc5a864f1
KamenevArtem <kamenevart94@gmail.com>
2023-12-01 01:50:39 +0300

    Update vk_bot.py

--- vk_bot.py
+++ vk_bot.py
@@ -1,35 +1,99 @@
 import random
 import os
 
+import redis
+
 from dotenv import load_dotenv
 
 import vk_api as vk
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.longpoll import VkLongPoll, VkEventType
 
+from parse_file import create_parsed_description
 
-def echo(event, vk_api):
-    keyboard = VkKeyboard(one_time=True)
-    keyboard.add_button('Новый вопрос', color=VkKeyboardColor.PRIMARY)
-    keyboard.add_button('Сдаться', color=VkKeyboardColor.PRIMARY)
-    keyboard.add_line()
-    keyboard.add_button('Посмотреть счёт', color=VkKeyboardColor.PRIMARY)
+
+def handle_new_question(event, vk_api, data_base, quiz_dict, title, keyboard):
+    random_task = random.choice(quiz_dict[title])
+    question, answer = random_task.values()
+    pipe = data_base.pipeline()
+    pipe.set(event.user_id, question)
+    pipe.set(question, answer[0])
+    pipe.execute()
+    print(answer[0])
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
+        message=question,
         random_id=random.randint(1,1000),
         keyboard=keyboard.get_keyboard()
     )
 
 
-def main():
-    load_dotenv()
-    vk_session = vk.VkApi(token=os.environ['VK_API_KEY'])
+def handle_surrender(event, vk_api):
+    vk_api.messages.send(
+                user_id=event.user_id,
+                message='Спасибо за уделенное Вами время. '
+                'Если хотите начать заново, введите команду: '
+                '"Начать" или "Заново"',
+                random_id=random.randint(1,1000),
+            )
+
+
+def handle_correct_answer(event, vk_api, keyboard):
+    vk_api.messages.send(
+                user_id=event.user_id,
+                message="Правильно",
+                random_id=random.randint(1,1000),
+                keyboard=keyboard.get_keyboard()
+            )
+
+
+def handle_start(event, vk_api, keyboard):
+    vk_api.messages.send(
+                user_id=event.user_id,
+                message='Здравствуйте! Выберете, что Вас интересует.',
+                random_id=random.randint(1,1000),
+                keyboard=keyboard.get_keyboard()
+            )
+
+
+def handle_unknown(event, vk_api):
+    vk_api.messages.send(
+                user_id=event.user_id,
+                message='Неверный ответ. Если Вам не был выслан вопрос,'
+                ' напишите "Начать" или "Заново"',
+                random_id=random.randint(1,1000),
+            )
+
+
+def vk_bot(vk_token):
+    vk_session = vk.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
+    data_base = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+    quiz_dict, title = create_parsed_description()
+    keyboard = VkKeyboard(one_time=True)
+    keyboard.add_button('Новый вопрос', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button('Сдаться', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_line()
+    keyboard.add_button('Посмотреть счёт', color=VkKeyboardColor.PRIMARY)
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+            if event.text == "Новый вопрос":
+                handle_new_question(event, vk_api, data_base, quiz_dict, title, keyboard)
+            elif event.text == "Сдаться":
+                handle_surrender(event, vk_api)
+            elif event.text in data_base.get(data_base.get(event.user_id)):
+                handle_correct_answer(event, vk_api, keyboard)
+            elif event.text in ["Начать", "Заново"]:
+                handle_start(event, vk_api, keyboard)
+            else:
+                handle_unknown(event, vk_api)
+
+
+def main():
+    load_dotenv()
+    vk_token = os.environ['VK_API_KEY']
+    vk_bot(vk_token)
 
 
 if __name__ == "__main__":
